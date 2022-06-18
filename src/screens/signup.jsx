f87fce9333348f0647af8a2e6b7b220be75dfc8e
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-06-19 00:46:18 +0600

    signup-page

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -13,19 +13,39 @@ import { colors } from "../theme/colors";
 import { spacing } from "../theme/spacing";
 
 export default function Signup() {
+  const selected = true;
+
   return (
     <SafeAreaView>
       <View style={{ padding: spacing[4] }}>
         <TextInput placeholder="Full Name" style={styles.input} />
+        <TextInput placeholder="Email" style={styles.input} />
         <TextInput
           placeholder="Password"
           style={styles.input}
           secureTextEntry
         />
-        <TextInput placeholder="Email" style={styles.input} />
         <TextInput placeholder="Phone" style={styles.input} />
         <TextInput placeholder="Age" style={styles.input} />
-        <TextInput placeholder="Gender" style={styles.input} />
+        <Pressable style={styles.radioContainer}>
+          <View style={styles.outerCircle}>
+            <View style={styles.innerCircle} />
+          </View>
+          <Text style={styles.radioText}>Male</Text>
+        </Pressable>
+        <Pressable style={styles.radioContainer}>
+          <View
+            style={[styles.outerCircle, selected && styles.selectedOuterCircle]}
+          >
+            <View
+              style={[
+                styles.innerCircle,
+                selected && styles.selectedInnerCircle,
+              ]}
+            />
+          </View>
+          <Text style={styles.radioText}>Female</Text>
+        </Pressable>
         <Button
           title={"Sign Up"}
           customStyles={{ alignSelf: "center", marginTop: spacing[10] }}
@@ -56,5 +76,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 30,
+  },
+  radioContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  outerCircle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#cfcfcf",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innerCircle: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#cfcfcf",
+  },
+  radioText: {
+    marginLeft: spacing[2],
+  },
+  selectedOuterCircle: {
+    borderColor: colors.orange,
+  },
+  selectedInnerCircle: {
+    borderColor: colors.orange,
+    backgroundColor: colors.orange,
   },
 });
