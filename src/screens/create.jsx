374bff25e8287f95316bf43ca635e077c861f7ca
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-06-22 23:15:04 +0600

    create layout

--- src/screens/create.jsx
+++ src/screens/create.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { spacing } from "../theme/spacing";
+import { TextInput } from "react-native";
+import { colors } from "../theme/colors";
 
-export default function Create() {
-    return (
-        <View>
-            <Text>Create</Text>
-        </View>
-    )
+export default function Create({ navigation, route, user }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <SafeAreaView style={{ flex: 1, marginHorizontal: 20 }}>
+      <TextInput
+        placeholder="Title"
+        onChangeText={(text) => setTitle(text)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Description"
+        multiline={true}
+        onChangeText={(text) => setDescription(text)}
+        style={styles.input}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 48,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.grey,
+    paddingHorizontal: spacing[3],
+    marginBottom: spacing[5],
+  },
+});
